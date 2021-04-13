c1585e57518d9391326729e85ebf3f3ccdbd4590
Dark Boi <66882633+darkdarcool@users.noreply.github.com>
2021-04-13 09:53:42 -0700

    Update sp.js

--- src/sp.js
+++ src/sp.js
@@ -28,14 +28,14 @@ function sp(str, speed){
 function die(reason) {
     throw new Error(reason);
 };
-
+/*
 let red = "\x1B[31m";
 let blue = "\x1B[34m";
 let bold = "\x1B[1m";
 let reset = "\x1B[0m";
 let green = "\x1B[32m";
 let yellow = '\x1b[33m';
-
+*/
 module.exports = {
   sleep: sleep,
   sp: sp,
